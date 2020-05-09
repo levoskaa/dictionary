977e272495a8c5c792825824a587c1dc6e430308
levoskaa <laurinyecz.levi@gmail.com>
2020-05-09 22:15:26 +0200

    Create base of WordPage

---
+++ Dictionary/Views/WordPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Dictionary.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Dictionary.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WordPage : Page
+    {
+        public WordPageViewModel ViewModel { get; private set; }
+
+        public WordPage()
+        {
+            this.InitializeComponent();
+            ViewModel = new WordPageViewModel();
+            DataContext = ViewModel;
+            this.Loaded += new RoutedEventHandler(PageLoadedHandler);
+        }
+
+        private void PageLoadedHandler(object sender, RoutedEventArgs e)
+        {
+            ViewModel.LoadData();
+        }
+    }
+}
