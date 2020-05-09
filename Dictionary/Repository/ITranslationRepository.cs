faabd38b4c3b1163df881b522ed3ad49b876d01a
levoskaa <laurinyecz.levi@gmail.com>
2020-05-09 22:14:30 +0200

    Create TranslationRepository

---
+++ Dictionary/Repository/ITranslationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dictionary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dictionary.Repository
+{
+    public interface ITranslationRepository
+    {
+        Task<IEnumerable<string>> GetLanguagesAsync();
+
+        Task<Translation> GetTranslationAsync(string word, string fromLanguage, string toLanguage);
+    }
+}
---
+++ Dictionary/Repository/RestTranslationRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dictionary.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dictionary.Repository
+{
+    public class RestTranslationRepository : ITranslationRepository
+    {
+        private static readonly HttpClient httpClient = new HttpClient { BaseAddress = new Uri("https://dictionary.yandex.net/api/v1/dicservice.json/") };
+        private readonly string apiKey = "dict.1.1.20200508T120242Z.0190a48daee8e510.c6cc8065cc43e19df744a91a403def65c5b7123b";
+
+        public async Task<IEnumerable<string>> GetLanguagesAsync()
+        {
+            var response = await httpClient.GetAsync($"getLangs?key={apiKey}");
+            string json = await response.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<IEnumerable<string>>(json);
+            return obj;
+        }
+
+        public async Task<Translation> GetTranslationAsync(string word, string fromLanguage, string toLanguage)
+        {
+            var response = await httpClient.GetAsync($"lookup?key={apiKey}&lang={fromLanguage}-{toLanguage}&text={word}");
+            string json = await response.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<Translation>(json);
+            return obj;
+        }
+    }
+}
