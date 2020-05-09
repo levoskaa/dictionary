2227dda5e331eac98ba346895c024210f1b20818
levoskaa <laurinyecz.levi@gmail.com>
2020-05-09 22:15:00 +0200

    Create BooleanToVisibilityConverter

---
+++ Dictionary/Resources/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Dictionary.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType != typeof(Visibility))
+            {
+                throw new InvalidOperationException("Target type must be Visibility!");
+            }
+            return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
