61c9d9ed24613ac357c400563c2b0749d24f5aa5
levoskaa <laurinyecz.levi@gmail.com>
2020-05-10 12:50:38 +0200

    Create Translation

---
+++ Dictionary/Models/Translation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Dictionary.Models
+{
+    public class Translation
+    {
+        public string SourceWord { get; set; }
+
+        public string TranslatedWord { get; set; }
+
+        public List<string> Synonyms { get; set; }
+    }
+}
